cdd01b090304d19a7dba809f0c44e30efcb60cb6
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-10-30 22:55:42 -0400

    more exercises

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function divide (a,b){
 }
 
 divide(6,7);
+
+function isLarger(a,b){
+    a>b ? console.log("a: ",a) : console.log("b: ",b);
+}
+
+var firstFraction = 1/4;
+var secondFraction = 3/4;
+
+isLarger(firstFraction,secondFraction);
+isLarger(5/6,8/9);
+isLarger(1/2,2/5);
+
+
+function largerFraction(a,b){
+    var result;
+    a>b ? result = ["firstFraction",a] : result = ["secondFraction",b];
+    return result;
+}
+
+var fractionResult = largerFraction(firstFraction,secondFraction);
+
+console.log("First Fraction Result", firstFraction);
+console.log("Second Fraction Result",secondFraction);
+console.log("Fraction "+ fractionResult[0] + "with a value of " + fractionResult[1]+"is the largest!");
